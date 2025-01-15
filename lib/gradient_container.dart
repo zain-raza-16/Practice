98f085b5a34bb9586b86960425eb548535c9bfd0
Zain Raza <71686254+zain-raza-16@users.noreply.github.com>
2025-01-15 11:35:56 -0600

    added Images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -7,13 +7,28 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [Colors.amberAccent, Colors.red]),
-      ),
-      child: const Center(
-        child: TextWhite(),
-      ),
-    );
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.amberAccent, Colors.red]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                "assets/images/dice-1.png",
+                width: 200,
+              ),
+              const SizedBox(width: 50,height: 50,),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                    // padding: EdgeInsets.all(100),
+                    foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    textStyle: const TextStyle(fontSize: 28)),
+                child: const Text("click me"),
+              )
+            ],
+          ),
+        ));
   }
 }
-
